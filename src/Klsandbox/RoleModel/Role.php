c3bfc90ae40aa90443235b30e47f602460568e4b
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2015-07-23 21:48:20 -0700

    simple role model

---
+++ src/Klsandbox/RoleModel/Role.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Klsandbox\SiteModel\Site;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Role
+ *
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\User[] $users
+ * @property integer $site_id
+ * @property integer $id
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @property string $name
+ * @property string $friendly_name
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Role whereSiteId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Role whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Role whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Role whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Role whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Models\Role whereFriendlyName($value)
+ */
+class Role extends Model {
+
+    use \Klsandbox\SiteModel\SiteExtensions;
+
+    protected $table = 'roles';
+    public $timestamps = true;
+
+    public function users() {
+        return $this->hasMany('App\Models\User');
+    }
+
+    public static function findByName($roleName) {
+        return Role::firstByAttributes(['name' => $roleName, 'site_id' => Site::id()]);
+    }
+
+    public static function __callStatic($method, $parameters) {
+        if (in_array($method, ['Stockist', 'Admin'])) {
+            return Role::firstByAttributes(['name' => $method, 'site_id' => Site::id()]);
+        }
+
+        return parent::__callStatic($method, $parameters);
+    }
+
+}
---
+++ src/Klsandbox/RoleModel/RoleModelServiceProvider.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Klsandbox\RoleModel;
+
+use Illuminate\Support\ServiceProvider;
+
+class RoleModelServiceProvider extends ServiceProvider {
+
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register() {
+        //
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides() {
+        return [];
+    }
+
+    public function boot() {
+        $this->publishes([
+            __DIR__ . '/../../../database/migrations/' => database_path('/migrations')
+                ], 'migrations');
+
+        $this->publishes([
+            __DIR__ . '/../../../config/' => config_path()
+                ], 'config');
+    }
+
+}
