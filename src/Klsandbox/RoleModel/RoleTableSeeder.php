cbc5fa1fa4e442708c2746e039aefc813287ec60
Ibrahim Abdul Rahim <mrwaim@gmail.com>
2016-03-22 17:35:45 +0800

    fix when name is inconsistent

--- src/Klsandbox/RoleModel/RoleTableSeeder.php
+++ src/Klsandbox/RoleModel/RoleTableSeeder.php
@@ -3,23 +3,25 @@
 namespace Klsandbox\RoleModel;
 
 use Illuminate\Database\Seeder;
-use Klsandbox\RoleModel\Role;
 use Klsandbox\SiteModel\Site;
 
-class RoleTableSeeder extends Seeder {
+class RoleTableSeeder extends Seeder
+{
 
-    public function run() {
+    public function run()
+    {
         $roles = \Config::get('role.roles');
 
         foreach (Site::all() as $site) {
             Site::setSite($site);
             foreach ($roles as $roleName => $roleFriendlyName) {
-                $role = Role::firstOrNew(array(
-                    'name' => $roleName,
-                    'friendly_name' => $roleFriendlyName
-                ));
 
-                $role->save();
+                $role = Role::forSite()->where('name', '=', $roleName);
+                if (!$role) {
+                    Role::create(['name' => $roleName,
+                        'friendly_name' => $roleFriendlyName]);
+                    $role->save();
+                }
             }
         }
     }
